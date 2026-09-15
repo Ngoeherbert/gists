a0d0f56f552f120bc1964ec29676cdef9775614e
Ngoeherbert <ngoeherbert237@gmail.com>
2026-09-15 06:51:28 +0100

    fix: remove duplicate exports in common index file

--- components/common/index.js
+++ components/common/index.js
@@ -3,3 +3,8 @@
 
 export { default as Screen } from "./Screen";
 export { default as Header } from "./Header";
+// components/common/index.js
+// Barrel for layout-level shared components.
+
+export { default as Screen } from "./Screen";
+export { default as Header } from "./Header";
